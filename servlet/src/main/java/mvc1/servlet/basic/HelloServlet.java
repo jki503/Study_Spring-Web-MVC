8b1d700d77f2dfd324fc1bb3115d1f780a677b59
jki503 <jki503@naver.com>
2022-01-25 16:54:53 +0900

    create project and source code about httpServlet basic

---
+++ servlet/src/main/java/mvc1/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package mvc1.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet{
+
+    // servlet 호출 되면 service 함수 호출.
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+
+
+    }
+}
